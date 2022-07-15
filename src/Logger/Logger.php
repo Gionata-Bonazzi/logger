ad5e9c0fa976697d50f572d00803f7430f3e8b72
Gionata Bonazzi <gionata.bonazzi@up3up.it>
2022-07-15 11:34:18 +0200

    Cambiato la classe per estendere monolog direttamente

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -19,58 +19,36 @@ use Up3Up\Logger\Handlers\PHPMailerHandler;
  * ALERT (550): Action must be taken immediately. Example: Entire website down, database unavailable, etc. This should trigger the SMS alerts and wake you up.
  * EMERGENCY (600): Emergency: system is unusable.
  */
-class Logger {
-    /** @var \Monolog\Logger */
-    protected $logger;
-    protected $levels;
-    protected $channel;
+class Logger extends \Monolog\Logger{
 
-    /**
-     * Crea un log, in tre formati:
-     * - file di testo
-     * - tabella su un database
-     * - email.
-     * Almeno uno dei tre formati deve essere fornito, altrimenti viene creato un log testuale nella cartella corrente con nome $level-$channel-log.log.
-     */
     public function __construct($channel)
     {
-        $this->logger = new \Monolog\Logger($channel);
-        $this->levels = $this->logger->getLevels();
-    }
-
-    public function getLogger() {
-        return $this->logger;
+        parent::__construct($channel);
     }
 
     public function pushEmailHandler($emailer, $level) {
         $result = false;
-        if($this->checkLevel($level)) {
-            if($emailer instanceof PHPMailer) {
-                $this->logger->pushHandler(new PHPMailerHandler($emailer, $this->level[$level]));
-                $result = true;
-            }
+        if($emailer instanceof PHPMailer) {
+            $this->pushHandler(new PHPMailerHandler($emailer, $level));
+            $result = true;
         }
         return $result;
     }
 
     public function pushPDOHandler($db, $level) {
         $result = false;
-        if($this->checkLevel($level)) {
-            if($db instanceof PDO) {
-                $this->logger->pushHandler(new PDOHandler($db, $this->level[$level]));
-                $result = true;
-            }
+        if($db instanceof PDO) {
+            $this->pushHandler(new PDOHandler($db, $level));
+            $result = true;
         }
         return $result;
     }
 
     public function pushTextHandler($file, $level) {
         $result = false;
-        if($this->checkLevel($level)) {
-            if(!empty($file)) {
-                $this->logger->pushHandler(new StreamHandler($file, $this->level[$level]));
-                $result = true;
-            }
+        if(!empty($file)) {
+            $this->pushHandler(new StreamHandler($file, $level));
+            $result = true;
         }
         return $result;
     }
@@ -78,16 +56,16 @@ class Logger {
     public function checkLevel($level) {
         return in_array($level, array_keys($this->levels));
     }
-    
+
     /**
      * Restituisce un oggetto monolog\Logger, invece di avere l'oggetto Up3Up\Logger, già preparato con gli handler collegati al livello passato.
      * Questo permette di creare un logger con solo un livello minimo di log per tutti gli handler.
      */
     public static function get_logger($channel, $level, $file_log=null, $db=null, $emailer=null) {
-        $logger = new Logger($channel, $level, $file_log, $db, $emailer);
+        $logger = new Logger($channel);
         $logger->pushEmailHandler($emailer, $level);
         $logger->pushPDOHandler($db, $level);
         $logger->pushTextHandler($file_log, $level);
-        return $logger->getLogger();
+        return $logger;
     }
 }
